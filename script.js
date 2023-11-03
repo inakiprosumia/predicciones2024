e0bd413e9fcd0eed2c2fdff3ecc94007a90d4ed7
inakiprosumia <149790708+inakiprosumia@users.noreply.github.com>
2023-11-03 10:13:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const cartas = [
   {
-    titulo: "En el 2024, el Día del Amigo será aún más especial",
+    titulo: "En el 2024, el Día del Amigo será aún más especial con un nuevo evento sorpresa",
   },
   {
-    titulo: "En el 2024, la Fiesta de fin de año será inolvidable",
+    titulo: "En el 2024, la Fiesta de fin de año será inolvidable con una nueva temática",
   },
   {
-    titulo: "En el 2024, las Pascuas serán llenas de alegría",
+    titulo: "En el 2024, las Pascuas serán llenas de alegría con un nuevo juego para los niños",
   },
   {
-    titulo: "En el 2024, los almuerzos de trabajo serán más divertidos",
+    titulo: "En el 2024, los almuerzos de trabajo serán más divertidos con un nuevo menú",
   },
   {
-    titulo: "En el 2024, las charlas de diseño serán más inspiradoras",
+    titulo: "En el 2024, las charlas de diseño serán más inspiradoras con un nuevo invitado",
   },
   {
-    titulo: "En el 2024, el Día del trabajador será un día de descanso merecido",
+    titulo: "En el 2024, el Día del trabajador será un día de descanso merecido con un nuevo regalo",
   },
   {
-    titulo: "En el 2024, las charlas sobre mobbing serán más efectivas",
+    titulo: "En el 2024, las charlas sobre mobbing serán más efectivas con un nuevo enfoque",
   },
   {
-    titulo: "En el 2024, las charlas para líderes de Prosumia serán más enriquecedoras",
+    titulo: "En el 2024, las charlas para líderes de Prosumia serán más enriquecedoras con un nuevo formato",
   },
 ];
 
@@ -35,6 +35,12 @@ const generarCartas = () => {
       </div>
     );
   });
+
+  document.querySelector(".cartas").innerHTML = cartasHTML.join("");
+};
+
+document.querySelector("#generar").addEventListener("click", generarCartas);
+
 
   document.querySelector(".cartas").innerHTML = cartasHTML.join("");
 };
